e6d0828775ddf178af9621bffc620b7e8b8d4656
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-06-23 11:11:48 -0700

    [empdir] android BaseDir (todo)

---
+++ RestaurantGuide/Android/BaseUrl_Android.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using RestaurantGuide.Android;
+
+[assembly: Dependency (typeof (BaseUrl_Android))]
+
+namespace RestaurantGuide.Android
+{
+	public class BaseUrl_Android : IBaseUrl
+	{
+		#region IBaseUrl implementation
+
+		public string Get ()
+		{
+			return NotImplementedException ("Once bug is fixed in Xamarin.Forms, implement this!"); 
+		}
+
+		#endregion
+	}
+}
+

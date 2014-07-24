68668ded4e9bba0cccf4e6898a3091771ad17861
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-23 22:17:27 -0700

    [oxyplot] fix menu (sort & make hierarchy)

--- OxyPlotDemo/OxyPlotDemo/ExampleList.cs
+++ OxyPlotDemo/OxyPlotDemo/ExampleList.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using System.Collections.Generic;
 using ExampleLibrary;
+using System.Linq;
 
 namespace OxyPlotDemo
 {
@@ -11,22 +12,63 @@ namespace OxyPlotDemo
 
 		public ExampleList ()
 		{
-			// TODO: maybe i'll, you know, sort and group this a bit...
 			exampleInfoList = ExampleLibrary.Examples.GetList();
 
-			var listView = new ListView ();
-			listView.ItemTemplate = new DataTemplate (typeof(TextCell));
-			listView.ItemsSource = exampleInfoList;
-			listView.ItemTemplate.SetBinding (TextCell.TextProperty, "Title");
-			listView.ItemSelected += (sender, e) => {
-				var ei = (ExampleInfo)e.SelectedItem;
-				var g = new GraphViewPage(ei);
-				g.Title = ei.Title;
-				Navigation.PushAsync(g);
-			};
+			var tableView = new TableView ();
+			var root = new TableRoot ("OxyPlot Example Browser");
+			var section = new TableSection ();
+			section.Add (exampleInfoList
+				.GroupBy (e => e.Category)
+				.OrderBy (g => g.Key)
+				.Select (g => GetCategoryCell (g.Key)));
+			root.Add (section);
+			tableView.Root = root;
+
 			Content = new StackLayout {
-				Children = {listView}
+				Children = {tableView}
 			};
+		}
+
+		Cell GetCategoryCell (string cat) {
+			var cell = new TextCell {Text = cat};
+			cell.Tapped += (sender, ea) => {;
+				var category = cat;
+
+				var tableView = new TableView ();
+				var root = new TableRoot (category);
+				var section = new TableSection ();
+				section.Add (exampleInfoList
+					.Where (e => e.Category == category)
+					.OrderBy (e => e.Title)
+					.Select (e => GetGraphCell(e.Title) ));
+				root.Add (section);
+				tableView.Root = root;
+
+				var contentPage = new ContentPage {
+					Title = category,
+					Content = new StackLayout {
+						Children = {tableView}
+					}
+				};
+
+				Navigation.PushAsync (contentPage);
+			};
+			return cell;
+		}
+
+		Cell GetGraphCell (string title)
+		{
+			var cell = new TextCell { Text = title };
+			cell.Tapped += (sender, ea) => {
+				var ei = exampleInfoList
+					.Where (e => e.Title == title)
+					.FirstOrDefault ();
+
+				var gp = new GraphViewPage (ei){Title = title};
+
+				Navigation.PushAsync (gp);
+			};
+			return cell;
 		}
 	}
 }

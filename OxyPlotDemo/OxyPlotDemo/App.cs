c2b24c20f5788068f6ae4347d16281add69f7000
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-23 20:01:09 -0700

    [oxyplot] add example from forums

--- OxyPlotDemo/OxyPlotDemo/App.cs
+++ OxyPlotDemo/OxyPlotDemo/App.cs
@@ -12,6 +12,11 @@ namespace OxyPlotDemo
 		{	
 			return new NavigationPage(new ExampleList {Title = "OxyPlot Examples" });
 
+			// 
+			// http://forums.xamarin.com/discussion/21031/oxyplot-chart-inside-a-contentpage#latest
+			//
+//			return new BasicSquareWave ();
+
 			//
 			// this was the original test code
 			//
---
+++ OxyPlotDemo/OxyPlotDemo/BasicSquareWave.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xamarin.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+
+namespace OxyPlotDemo
+{
+	/// <summary>
+	/// chart data from OxyPlot WPF example
+	/// https://oxyplot.codeplex.com/wikipage?title=WpfExample1
+	/// </summary>
+	public class BasicSquareWave : ContentPage
+	{
+		public BasicSquareWave ()
+		{	this.Title = "Gráfico";
+			this.Icon = "Icon.png";
+
+			PlotModel myModel = new PlotModel("Square wave");
+			var ls = new LineSeries("sin(x)+sin(3x)/3+sin(5x)/5+...");
+			int n = 10;
+			for (double x = -10; x < 10; x += 0.0001)
+			{
+				double y = 0;
+				for (int i = 0; i < n; i++)
+				{
+					int j = i * 2 + 1;
+					y += Math.Sin(j * x) / j;
+				}
+				ls.Points.Add(new DataPoint(x, y));
+			}
+			myModel.Series.Add(ls);
+			myModel.Axes.Add(new LinearAxis(AxisPosition.Left, -4, 4));
+			myModel.Axes.Add(new LinearAxis(AxisPosition.Bottom));
+
+			var opv = new OxyPlotView {WidthRequest = 300, HeightRequest = 300};
+			opv.Model = myModel;
+
+			this.Content = new StackLayout
+			{
+				Spacing = 20,
+				Padding = 10,
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					opv
+				}
+			};
+		}
+	}
+}
+

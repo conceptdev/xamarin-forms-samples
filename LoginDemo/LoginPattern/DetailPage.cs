f9ff5549215f0f8579ef5f54a8a0d084c5c0812c
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-25 15:20:56 -0700

    [login] add WinPhone example

--- LoginDemo/LoginPattern/DetailPage.cs
+++ LoginDemo/LoginPattern/DetailPage.cs
@@ -9,13 +9,17 @@ namespace LoginPattern
 		{
 			BackgroundColor = new Color (0, 0, 1, 0.2);
 
+            var text = "Slide > to see the master / menu";
+            if (Device.OS == TargetPlatform.WinPhone) {
+                text = @"Click button \/ to see the master / menu ";
+            }
 
 			Content = new StackLayout { 
 				HorizontalOptions = LayoutOptions.Center,
 				Padding = new Thickness (10, 40, 10, 10),
 				Children = {
 
-					new Label {Text = "Slide > to see the master / menu"}
+					new Label { Text = text }
 				}
 			};
 		}
---
+++ LoginDemo/WinPhone/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace LoginPattern.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage, ILoginManager
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = LoginPattern.App.GetLoginPage(this).ConvertPageToUIElement(this);
+        }
+
+        public void ShowMainPage()
+        {
+            Content = LoginPattern.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+
+        public void Logout()
+        {
+            Content = LoginPattern.App.GetLoginPage(this).ConvertPageToUIElement(this);
+        }
+    }
+}

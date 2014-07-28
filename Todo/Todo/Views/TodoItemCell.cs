d67f896e79cb136052c8b7937979b6e653c2170c
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-28 14:15:33 -0700

    [todo] removed unused code

--- Todo/Todo/Views/TodoItemCell.cs
+++ Todo/Todo/Views/TodoItemCell.cs
@@ -26,15 +26,6 @@ namespace Todo
 			};
 			View = layout;
 		}
-
-		protected override void OnBindingContextChanged ()
-		{
-			// Fixme : this is happening because the View.Parent is getting 
-			// set after the Cell gets the binding context set on it. Then it is inheriting
-			// the parents binding context.
-			View.BindingContext = BindingContext;
-			base.OnBindingContextChanged ();
-		}
 	}
 }
 

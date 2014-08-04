5b12854c668a72f66e87ed0164235bd98482c260
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-04 14:33:30 -0700

    [L10n] make WinPhone work (update X.F too)

---
+++ TodoL10nResx/PCL/Todo.WinPhone/Locale_WinPhone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Todo.WinPhone.Locale_WinPhone))]
+
+namespace Todo.WinPhone
+{
+    public class Locale_WinPhone : Todo.ILocale
+    {
+        /// <remarks>
+        /// Not sure if we can cache this info rather than querying every time
+        /// </remarks>
+        public string GetCurrent()
+        {
+            var lang = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+            var culture = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+            return lang;
+        }
+    }
+}

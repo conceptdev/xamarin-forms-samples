eebc931921631e3a69d343016e5a66fee5a776ad
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-09-25 14:07:30 -0700

    [evolve] get WP running (no data tho)

---
+++ Evolve13/Evolve13.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Evolve13.WinPhone.Resources;
+
+namespace Evolve13.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Evolve13/Evolve13.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+using System.IO;
+
+
+namespace Evolve13.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+            var sqliteFilename = "Evolve.db3";
+            var path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, sqliteFilename); ;
+
+            var plat = new SQLite.Net.Platform.WindowsPhone8.SQLitePlatformWP8();
+            var conn = new SQLite.Net.SQLiteConnection(plat, path);
+
+            // Set the database connection string
+            Evolve13.App.SetDatabaseConnection(conn);
+
+
+            Content = Evolve13.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}

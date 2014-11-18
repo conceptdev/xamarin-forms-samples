c31617fb79eb0a5a9c5602151422ccc644e21ea3
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-11-17 17:41:05 -0800

    [todo] UITest updates

--- Todo/Todo.UITests/CrossPlatformTests.cs
+++ Todo/Todo.UITests/CrossPlatformTests.cs
@@ -10,7 +10,19 @@ namespace Todo.UITests
 	{
 		protected IApp _app;
 
-		// toolbar
+		bool IsAndroid {
+			get {
+				return _app is Xamarin.UITest.Android.AndroidApp;
+			}
+		}
+
+		bool IsiOS {
+			get {
+				return _app is Xamarin.UITest.iOS.iOSApp;
+			}
+		}
+
+		// toolbar - (BUG) can't tap these in iOS
 //		static readonly Func<AppQuery, AppQuery> Add = c => c.Marked("+");
 //		static readonly Func<AppQuery, AppQuery> SpeakAll = c => c.Marked("?");
 		static readonly Func<AppQuery, AppQuery> ToolbarAdd = c => c.Marked("ToolbarAdd");
@@ -45,25 +57,58 @@ namespace Todo.UITests
 			_app.Tap (Speak);
 			_app.Tap (Delete);
 		}
+
+		void AddFromList (string name, string notes) {
+			_app.Tap (AddButton);
+
+			_app.WaitForElement (TodoName);
+			_app.Tap (TodoName);
+			_app.EnterText (name);
+			_app.Tap (TodoNotes);
+			_app.EnterText (notes);
+			_app.Tap (Save); 
+		}
+
 		#endregion
 
 		[Test ()]
-		public void TestCase ()
+		public void TestDelete ()
 		{
-			//_app.Repl ();
+			// Arrange 
+			AppResult[] result = _app.Query (a => a.Marked ("Buy pears"));
+			Assert.IsTrue(result.Any(), "Expected there to be a 'buy pears' row to delete; remove the app from the simulator and re-run test.");
 
-//			var x = _app.Query ();
-//			var y = _app.Query (g => g.Marked ("TodoAdd"));
-//			var z = _app.Query (h => h.Marked ("TodoList"));
-//			var a = _app.Query (i => i.Marked ("CellLabel"));
+			// Act
+			TapAndDelete ("Buy pears");
 
+			// Assert
+			result = _app.Query(a => a.Marked ("Buy pears"));
+			Assert.IsTrue(result.Length == 0, "The 'buy pears' row wasn't deleted :-(");
+		}
+
+		[Test ()]
+		public void TestAdd ()
+		{
+			// Arrange 
+			AppResult[] result = _app.Query (a => a.Marked ("Buy kale"));
+			Assert.IsTrue(result.Length == 0, "There should not already be a 'buy kale' task");
+
+			// Act
+			AddFromList ("Buy kale", "organic"); 
+			_app.WaitForElement (AddButton); // back on listView page
+
+			// Assert
+			result = _app.Query(a => a.Marked ("Buy kale"));
+			Assert.IsTrue(result.Any(), "The 'buy kale' row wasn't added :-(");
+		}
+
+		[Test (), Category("DodgyTests")]
+		public void DoAll ()
+		{
 			// Arrange - Nothing to do because the queries have already been initialized.
-//			AppResult[] result = _app.Query(InitialMessage);
-//			Assert.IsTrue(result.Any(), "The initial message string isn't correct - maybe the app wasn't re-started?");
-//
-//			// Act
-//			_app.Tap(Add);
+			Assert.IsTrue(true, "Noop, this is for demo purposes only :-) ");
 
+			// Act
 			var aq = _app.Query (a => a.Marked ("Buy pears"));
 			if (aq.Length > 0) { // exists 
 				TapAndDelete ("Buy pears");
@@ -90,44 +135,19 @@ namespace Todo.UITests
 				_app.Tap (Delete);
 			}
 
-//			_app.Tap (ToolbarAdd); // on toolbar (BUG)
-			_app.Tap (AddButton); // on floating image button
-			_app.Tap (TodoName);
-			_app.EnterText ("Buy kale");
-			_app.Tap (TodoNotes);
-			_app.EnterText ("organic");
-			_app.Tap (TodoDone);
-			_app.Tap (Save); 
 
 
+			AddFromList ("Buy kale", "organic"); 
+			AddFromList ("Buy broccolini", "organic"); 
+			AddFromList ("Buy dragonfruit", "organic"); 
 
+			if (IsAndroid) {
+				_app.Tap (SpeakAll); // on toolbar (BUG can't tap on iOS)
+			}
 
-			_app.Tap (AddButton);
-			_app.Tap (TodoName);
-			_app.EnterText ("Buy broccolini");
-			_app.Tap (TodoNotes);
-			_app.EnterText ("organic");
-			//_app.Tap (TodoDone);
-			_app.Tap (Save); 
-
-
-
-			_app.Tap(AddButton);
-			_app.Tap (TodoName);
-			_app.EnterText ("Buy dragonfruit");
-			_app.Tap (TodoNotes);
-			_app.EnterText ("organic");
-			_app.Tap (TodoDone);
-			_app.Tap (Save); 
-
-
-
-//			_app.Tap (SpeakAll); // on toolbar (BUG)
-
-
-//			// Assert
-//			result = _app.Query(DoneMessage);
-//			Assert.IsTrue(result.Any(), "The 'clicked' message is not being displayed.");
+			// Assert
+			var result = _app.Query(z => z.Marked ("Buy dragonfruit"));
+			Assert.IsTrue(result.Any(), "The 'dragonfruit' task wasn't added :-(");
 		}
 	}
 }

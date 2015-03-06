ac7f26ea50991c49555208bac2c8cb271146ba52
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-03-06 14:06:24 -0800

    [parallax] add screenshots

--- Parallax/Parallax/ParallaxPage.cs
+++ Parallax/Parallax/ParallaxPage.cs
@@ -4,6 +4,9 @@ using Xamarin.Forms;
 
 namespace Parallax
 {
+	/*
+	 *      WE RECOMMEND YOU TEST ON DEVICE
+	 */
 	public class ParallaxPage : ContentPage
 	{
 		public ParallaxPage ()
@@ -15,32 +18,111 @@ namespace Parallax
 				AnchorY = 0
 			};
 
-			var g = new Grid ();
-			var s = new ScrollView ();
-			s.Scrolled += (object sender, ScrolledEventArgs e) => {
+			// grid is used to position the labels ON TOP OF the image
+			var layeringGrid = new Grid ();
+
+			var outerScrollView = new ScrollView ();
+			outerScrollView.Scrolled += (object sender, ScrolledEventArgs e) => {
 				var imageHeight = img.Height * 2;
-				var scrollRegion = g.Height - s.Height;
-				var parallexRegion = imageHeight - s.Height;
-				img.TranslationY = s.ScrollY - parallexRegion * (s.ScrollY / scrollRegion);
+				var scrollRegion = layeringGrid.Height - outerScrollView.Height;
+				var parallexRegion = imageHeight - outerScrollView.Height;
+				img.TranslationY = outerScrollView.ScrollY - parallexRegion * (outerScrollView.ScrollY / scrollRegion);
 			};
 
+			outerScrollView.Content = layeringGrid;
 
-			s.Content = g;
+			layeringGrid.Children.Add (img);
 
-			g.Children.Add (img);
+			StackLayout stackOfLabels = new StackLayout ();
 
-			StackLayout l = new StackLayout ();
-
-			for (int i = 0; i < 50; i++)
+			for (int i = 0; i < parks.Length; i++)
 			{
-				l.Children.Add (new Label () { Text = "ewljrjwel weljrlkewjrw lkjrlkwejrlkwe", HeightRequest = 50 });
+				stackOfLabels.Children.Add (new Label () { Text = parks[i], HeightRequest = 50, 
+					FontSize = Device.OnPlatform (iOS: 30, Android:Device.GetNamedSize(NamedSize.Large, typeof(Label)), WinPhone:Device.GetNamedSize(NamedSize.Default, typeof(Label)) ),
+					TextColor = Device.OnPlatform(iOS:Color.White, Android:Color.White, WinPhone:Color.Default),
+					FontAttributes = FontAttributes.Bold
+				});
 			}
 
-			g.Children.Add (l);
+			layeringGrid.Children.Add (stackOfLabels);
 
 			// The root page of your application    
-			Content = s;
+			Content = outerScrollView;
+			/*
+			The visual tree will look something like this:
+	
+			* Content
+			    + ScrollView
+			       + Grid
+			          + StackLayout
+					     + Label(s)
+			          + Image
+
+			When the scrollview is scrolled, the image is translated manually
+			*/
+
 		}
+	
+		//http://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States
+		string[] parks = new string[] {"Acadia"
+			,"American Samoa"
+			,"Arches"
+			,"Badlands"
+			,"Big Bend"
+			,"Biscayne"
+			,"Black Canyon of the Gunnison"
+			,"Bryce Canyon"
+			,"Canyonlands"
+			,"Capitol Reef"
+			,"Carlsbad Caverns"
+			,"Channel Islands"
+			,"Congaree"
+			,"Crater Lake"
+			,"Cuyahoga Valley"
+			,"Death Valley"
+			,"Denali"
+			,"Dry Tortugas"
+			,"Everglades"
+			,"Gates of the Arctic"
+			,"Glacier"
+			,"Glacier Bay"
+			,"Grand Canyon"
+			,"Grand Teton"
+			,"Great Basin"
+			,"Great Sand Dunes"
+			,"Great Smoky Mountains"
+			,"Guadalupe Mountains"
+			,"Haleakalā"
+			,"Hawaii Volcanoes"
+			,"Hot Springs"
+			,"Isle Royale"
+			,"Joshua Tree"
+			,"Katmai"
+			,"Kenai Fjords"
+			,"Kings Canyon"
+			,"Kobuk Valley"
+			,"Lake Clark"
+			,"Lassen Volcanic"
+			,"Mammoth Cave"
+			,"Mesa Verde"
+			,"Mount Rainier"
+			,"North Cascades"
+			,"Olympic"
+			,"Petrified Forest"
+			,"Pinnacles"
+			,"Redwood"
+			,"Rocky Mountain"
+			,"Saguaro"
+			,"Sequoia"
+			,"Shenandoah"
+			,"Theodore Roosevelt"
+			,"Virgin Islands"
+			,"Voyageurs"
+			,"Wind Cave"
+			,"Wrangell –St. Elias"
+			,"Yellowstone"
+			,"Yosemite"
+			,"Zion"};
 	}
 }
 
--- Parallax/README.md
+++ Parallax/README.md
@@ -3,3 +3,6 @@ Parallax Hack
 
 Jason's [parellax hack](http://forums.xamarin.com/discussion/34711/parallax-hack-for-fun-and-no-profit) placed into a ready-to-run project.
 
+![](https://raw.githubusercontent.com/conceptdev/xamarin-forms-samples/master/ParallaxScreenshots/parallax-android-sml.png) ![](https://raw.githubusercontent.com/conceptdev/xamarin-forms-samples/master/Parallax/Screenshots/parallax-ios-sml.png)
+
+*national park names from [wikipedia](http://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States)*

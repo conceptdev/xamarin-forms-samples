2d90c46861ec004d57f598d3cf957f0e25e8727b
Xamarin Monkeys <xamarinmonkeys@outlook.com>
2018-10-30 18:50:23 +0530

    Custom popup added

---
+++ CustomPopup/XamarinCustomPopup/XamarinCustomPopup/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinCustomPopup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> items = new List<string> { "Xamarin.Forms", "Xamarin.iOS", "Xamarin.Android","Xamarin Monkeys" };
+            imgMonkey.Source = ImageSource.FromResource("XamarinCustomPopup.images.monkey-MVP.png");
+            imgPopup.Source = ImageSource.FromResource("XamarinCustomPopup.images.xammonkey.png");
+            sampleList.ItemsSource = items;
+        }
+
+        private void btnPopupButton_Clicked(object sender, EventArgs e)
+        {
+            popupImageView.IsVisible = true;
+            activityIndicator.IsRunning = true;
+           
+        }
+
+       
+    }
+}

0ee8c8a1845c140632fc2c970e34914457e5a92e
Craig Dunn <craig.dunn@microsoft.com>
2020-05-02 15:36:19 -0700

    add drag-and-drop effects

---
+++ GetStarted/Notes/MultiPage/Notes/DragAndDropEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public class DragAndDropEffect : RoutingEffect
+    {
+        public string Uri { get; set; }
+
+        public string Description { get; set; }
+
+        public DragAndDropEffect() : base("Notes.DragAndDropEffect")
+        {
+
+        }
+    }
+}

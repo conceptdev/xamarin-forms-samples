c6186d9265453505e787660cd20b37cfc2bcf071
Craig Dunn <craig.dunn@microsoft.com>
2020-05-06 12:57:07 -0700

    [notes] tidy up effect

--- GetStarted/Notes/MultiPage/Notes/ItemDragAndDropEffect.cs
+++ GetStarted/Notes/MultiPage/Notes/ItemDragAndDropEffect.cs
@@ -5,10 +5,6 @@ namespace Notes
 {
     public class ItemDragAndDropEffect : RoutingEffect
     {
-        public string Uri { get; set; }
-
-        public string Description { get; set; }
-
         public ItemDragAndDropEffect() : base("Notes.ItemDragAndDropEffect")
         {
 
